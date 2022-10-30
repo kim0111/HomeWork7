7c67b64f1e56b5a8fa09882425c8e603437f125a
Юра <@DESKTOP-4FA6BMM>
2022-10-30 17:19:15 +0600

    Добавьте файлы проекта.

---
+++ HomeWork7/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HomeWork7.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HomeWork7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public JsonResult GetJson()
+        {
+            using (FileStream fStream = new FileStream(@"C:\Users\Юра\source\repos\HomeWork7\HomeWork7\Files\users.json", FileMode.OpenOrCreate))
+            {
+                object? users = System.Text.Json.JsonSerializer.Deserialize<object>(fStream);
+                return Json(users);
+            }
+        }
+    }
+}
